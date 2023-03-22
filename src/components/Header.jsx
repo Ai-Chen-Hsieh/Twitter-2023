f4a73126d9a981677b02bc755af6c5fa9fd8edba
anna <b10332040@gmail.com>
2023-03-22 20:20:46 +0800

    [新增] header.styled.js 讓 Header.jsx 和 UserHeader.jsx 可以共用樣式

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
+import { HeaderContainer } from "./common/header.styled"
 
-const Header = () => {
+
+/**
+ * 頁首
+ * @param {string} text - 頁首標題文字 
+ * @returns 
+ */
+const Header = ({text=""}) => {
     return(
-        <></>
+        <HeaderContainer>
+            <h4>{text}</h4>
+        </HeaderContainer>
     )
 }
 
---
+++ src/components/common/header.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledHeaderContainer = styled.header`
+  width: 100%;
+  min-height: 74px;
+  padding-left: 24px;
+  padding-right: 24px;
+  border-bottom: 1px solid #E6ECF0;
+  background-color: #fff;
+`;
+
+export {
+  StyledHeaderContainer as HeaderContainer
+}
