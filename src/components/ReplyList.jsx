cb929837e045b89982d6ff184680e6159ceb9082
Ai-Chen-Hsieh <a0903109727@gmail.com>
2023-03-24 21:11:51 +0800

    create replyList

--- src/components/ReplyList.jsx
+++ src/components/ReplyList.jsx
@@ -1,7 +1,24 @@
+import styled from "styled-components"
+import { ReplyItem } from "."
+import { dummyUserReplyList } from "../testData/dummyUserReplyList"
+
+const StyledReplyList = styled.div`
+    width:100%;
+    min-height: 500px;
+`
 
 const ReplyList = () => {
     return (
-        <></>
+        <StyledReplyList>
+            {dummyUserReplyList.map(reply=>{
+                return(
+                    <ReplyItem
+                        key={reply.id}
+                        item={reply}
+                    />
+                )
+            })}
+        </StyledReplyList>
     )
 }
 
