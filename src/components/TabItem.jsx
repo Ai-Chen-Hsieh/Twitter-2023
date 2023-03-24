ddc235efa723bc1bc8254d4b629a0e3015441279
anna <b10332040@gmail.com>
2023-03-24 21:05:51 +0800

    [共用 Component]_TabItems+TabList 開發

--- src/components/TabItem.jsx
+++ src/components/TabItem.jsx
@@ -1,7 +1,44 @@
+import { NavLink } from "react-router-dom"
+import styled from "styled-components"
 
-const TabItem = () => {
+const StyledTabItemNavLink = styled(NavLink)`
+    display: block;
+    padding: 12px;
+    min-width: 90px;
+    max-width: 100%;
+    font-weight: 700;
+    text-decoration: none;
+    text-align: center;
+    color: var(--gray-100);
+    transition: color .5s;
+    &:hover,
+    &.active {
+        color: var(--main);
+    }
+    &.active {
+        border-bottom: 2px solid var(--main);
+        cursor: default;
+    }
+
+    @media screen and (min-width: 1200px){
+        padding: 16px;
+        min-width: 130px;
+    }
+`
+
+/**
+ * 頁籤項目
+ * @param {string} to - 連結
+ * @param {string} text - 連結文字 
+ * @returns 
+ */
+const TabItem = ({to, text}) => {
     return (
-        <></>
+        <li>
+            <StyledTabItemNavLink to={to} aria-label={text} title={text} end={true} >
+                {text}
+            </StyledTabItemNavLink>
+        </li>
     )
 }
 
