c703444a5e2198f89e4311bb35e2394d7b1cb50d
TammyKao <a0920760272@gmail.com>
2023-03-25 12:34:51 +0800

    revise layout

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -15,7 +15,7 @@ const StyledUserProfileContainer = styled.div`
   .background-image {
      width:100%;
      height:200px;
-     border: 1px solid red;
+    //  border: 1px solid red;
     img {
       width:100%;
       height:100%;
@@ -33,26 +33,30 @@ const StyledUserProfileContainer = styled.div`
     width: 95px;
     height: 48px;
   }
+`
 
-  .following, .follower{
-
-  }
-`;
-
-const StyledUserInfo = styled.div`
+const StyledUserInfoContainer = styled.div`
  padding:16px;
  width:100%;
  height:178px;
- border: 1px solid green;
+ position:relative;
+`
+
+const StyledUserInfo =styled.div`
+// border: 1px solid blue;
+ position:absolute;
+ bottom:0; 
 `
 
 const StyledFollowContainer = styled.div`
- border: 1px solid red;
+//  border: 1px solid red;
  font-size: var(--fz-secondary);
  display:flex;
+ .follower-link {
+  // border: 1px solid pink;
+  margin-left:20px;
+ }
 `
-
-
 
 const UserProfile = ({name='John Doe', account='@heyjhon', backgroundImageURL='https://cdn.sanity.io/images/0vv8moc6/dvm360/e9b7c5bc00c41167ab8d607d02e11385fba9dec5-5200x3550.jpg/AdobeStock_41610815.jpeg?w=1500&fit=max&auto=format', followingCount='20', followerCount='20'}) => {
     return (
@@ -66,23 +70,25 @@ const UserProfile = ({name='John Doe', account='@heyjhon', backgroundImageURL='h
           <div className ="userinfo-button">
             <Button text='編輯個人資料' styled='outlined'/>
           </div>
-        <StyledUserInfo>   
+        <StyledUserInfoContainer> 
+          <StyledUserInfo>  
           <div className ="user-info">
             <h5>{name}</h5>
             <p className="text-fz-secondary color-secondary">{account}</p>
           </div>
           <p className="text-fz-secondary">Qui similique aut voluptas explicabo illum impedit ut quod fugit.</p>
           <StyledFollowContainer>
-          <div className ="following">
-            <span className="following">{`${followingCount}個`}</span>
-            <span className="color-secondary">跟隨中</span>
+          <div className ="following-link">
+            <span className="following-count">{`${followingCount} 個`}</span>
+            <span className="following color-secondary">跟隨中</span>
           </div>
-          <div className ="follower">
-            <span className="follower">{`${followerCount}個`}</span>
-            <span className="color-secondary">跟隨者</span>
+          <div className ="follower-link">
+            <span className="follower-count">{`${followerCount} 個`}</span>
+            <span className="follower color-secondary">跟隨者</span>
           </div>
-        </StyledFollowContainer>   
-        </StyledUserInfo>          
+        </StyledFollowContainer>
+        </StyledUserInfo>   
+        </StyledUserInfoContainer>          
         </StyledUserProfileContainer>
     )
 };
---
+++ src/testData/dummyUserInfo.js
@@ -0,0 +1,19 @@
+const dummyUserInfo = [
+    {
+        id: 1,
+        name: "root",
+        email:"root@example.com",
+        image: "https://via.placeholder.com/300",
+        description: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint ullamco est sit aliqua dolor do sit aliqua dolor do amet sint ullamco est sit aliqua dolor do  sit aliqua dolor do amet sint ullamco est sit aliqua dolor do amet sint.",
+    },
+    {
+        id: 2,
+        name: "root2",
+        email:"root2@example.com",
+        image: "https://via.placeholder.com/300",
+        description: "156r mroif kfrpjt ",
+    }
+]
+
+export { dummyUserInfo }
+
