1cf3b4790e0b9c77056239a4fa211e2e7619f7a8
Ai-Chen-Hsieh <a0903109727@gmail.com>
2023-03-25 13:17:29 +0800

    create styledModal.js

--- src/components/common/modal.styled.js
+++ src/components/common/modal.styled.js
@@ -1,7 +1,88 @@
 import styled from "styled-components";
 
-const StyledModal = styled.div`
+const StyledModalContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    max-width: 650px;
+    max-height: 500px;
+    border:1px solid var(--gray-20); 
+    border-radius: 10px;
+`
+
+const StyledModalHeader = styled.div`
+    width: 100%;
+    height: 50px;
+    border-bottom: 1px solid var(--gray-20);
+    display: flex;
+    align-items: center;
+
+    .title{
+        display: inline-block;
+        line-height: 50px;
+        width: calc(100% - 130px);
+        margin-left: 5px;
+        font-size: 1.8rem;
+        font-weight: 700;
+    }
     
 `
 
-export { StyledModal as ModalContainer }
+const StyledCloseButton = styled.div`
+    display:inline-block;
+    width: 40px;
+    height: 40px;
+    margin-top: 4px;
+    margin-bottom: 4px;
+    margin-left: 5px;
+    border-radius: 50%;
+    color: var(--main);
+    position: relative;
+
+    &:hover {
+        background-color: var(--dark-30);
+    }
+
+    &:before {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        content: "\u0058";
+        color: var(--main);
+    }
+    
+`
+
+const StyledModalContent = styled.div`
+    width: 100%;
+    flex: 1;
+    
+`
+const StyledModalFooter = styled.div`
+    width: 100%;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding: 0 5px;
+
+    .warning{
+        display: inline-block;
+        line-height: 50px;
+        text-align: end;
+        width: calc(100% - 130px);
+        margin: 0 10px;
+        font-size: 1.5rem;
+        color: var(--error);
+    }
+`
+
+export { 
+    StyledModalContainer as ModalContainer,
+    StyledModalHeader as ModalHeader,
+    StyledCloseButton as ModalCloseButton,
+    StyledModalContent as ModalContent,
+    StyledModalFooter as ModalFooter
+}
--- src/pages/DemoPage.jsx
+++ src/pages/DemoPage.jsx
@@ -1,4 +1,6 @@
 import { Header } from "components";
+import { ModalContainer, ModalHeader, ModalCloseButton, ModalContent, ModalFooter } from "../components/common/modal.styled";
+import { Button } from "../components"
 
 /**
  * [Demo] 共用 Component
@@ -10,6 +12,43 @@ const DemoPage = () => {
       <div className="row">
         <div className="col-6">
           <Header text="首頁"/>
+          <ModalContainer>
+            <ModalHeader>
+              <ModalCloseButton/>
+              <span className="title">編輯個人資料</span>
+              <Button
+                text="儲存"
+              />
+            </ModalHeader>
+            <ModalContent></ModalContent>
+          </ModalContainer>
+          <br></br>
+          <ModalContainer>
+            <ModalHeader>
+              <ModalCloseButton/>
+            </ModalHeader>
+            <ModalContent></ModalContent>
+            <ModalFooter>
+              <span className="warning">字數不可超過140字</span>
+              <Button
+                text="推文"
+              />
+            </ModalFooter>
+          </ModalContainer>
+          <br></br>
+          <ModalContainer>
+            <ModalHeader>
+              <ModalCloseButton/>
+            </ModalHeader>
+            <ModalContent></ModalContent>
+            <ModalFooter>
+              <span className="warning">內容不可空白</span>
+              <Button
+                text="回覆"
+              />
+            </ModalFooter>
+          </ModalContainer>
+          <br />
         </div>
       </div>
     </div>
