b5fd4473ed1461171ee8013f5c72533756a0d9c1
Ai-Chen-Hsieh <a0903109727@gmail.com>
2023-03-27 22:49:42 +0800

    create component-UserTweet

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { createPortal } from "react-dom"
 import styled from "styled-components" 
-import { Header, TweetList, Avatar, Button, TweetModal } from "components"
+import { Header, TweetList, TweetModal, UserTweet } from "components"
 
 //測試假資料
 import { dummyAllTweet } from "../testData/dummyAllTweet"
@@ -15,40 +15,7 @@ const StyledMainContainer = styled.div`
     height:100%;
     border:1px solid var(--gray-20);
 `
-const StyledTweetContainer = styled.div`
-    width: 100%;
-    height: 150px;
-    padding: 10px 10px 10px 20px;
-    border-bottom: 8px solid var(--gray-20);
-    display: flex;
-`
 
-const StyledAvatar = styled.div`
-    width: 50px;
-    height: 50px;
-    border-radius: 50%;
-`
-
-const StyledTextContainer = styled.div`
-    margin-left: 10px;
-    margin-top: 10px;
-    flex: 1;
-    display:flex;
-    flex-direction: column;
-`
-
-const StyledText = styled.textarea`
-    resize: none;
-    border: none;
-    outline: none;
-    flex: 1;
-    font-weight: 700;
-    font-size: 1.4rem;
-`
-
-const StyledButton = styled.div`
-    align-self: end;
-`
 
 const MainPage = () => {
     const [ showTweetModal, setShowTweetModal ] = useState(false)
@@ -57,22 +24,10 @@ const MainPage = () => {
         <>
             <StyledMainContainer>
                 <Header text="首頁" />
-                    <StyledTweetContainer onClick={()=>setShowTweetModal(true)}>
-                        <StyledAvatar>
-                        <Avatar
-                            imageUrl="https://picsum.photos/200"/>
-                        </StyledAvatar>
-                        <StyledTextContainer>
-                            <StyledText 
-                                placeholder="有什麼新鮮事?"
-                            />
-                            <StyledButton>
-                                <Button
-                                    text="推文"
-                                />
-                            </StyledButton>
-                        </StyledTextContainer>
-                    </StyledTweetContainer>
+                    <UserTweet 
+                        item={dummyAllTweet[0].avatar}
+                        onClick={()=>setShowTweetModal(true)}
+                    />
                 <TweetList allTweets = {dummyAllTweet}/>
             </StyledMainContainer>
             {showTweetModal && createPortal(
