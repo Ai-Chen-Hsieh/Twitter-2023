79444814ee5d6fddbe6c213058c96af83123bee2
Ai-Chen-Hsieh <a0903109727@gmail.com>
2023-03-27 23:59:30 +0800

    create userPageTweet

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,7 +1,16 @@
+import styled from "styled-components"
+
+const StyledTweetContainer = styled.div`
+    border:1px solid red;
+    min-height: 400px;
+    display: flex;
+`
 
 const Tweet = () => {
     return (
-        <></>
+        <StyledTweetContainer>
+
+        </StyledTweetContainer>
     )
 }
 
---
+++ src/testData/dummyTweet.js
@@ -0,0 +1,15 @@
+
+const dummyTweet = {
+    status: "success",
+    message: "Successfully retrieved this Tweet.",
+    id: 1,
+    UserId: 2,
+    description: "Suscipit fuga eveniet vel impedit perspiciatis alias. Ab repellat tempora iste quia iste soluta. Perferendis sequi omnis. Libero dolor accusantium dicta voluptates.\n \rCorporis neque voluptates sed rerum omnis. Officia quidem consectetur sint doloribus eum et minima illo aut. Veritatis eius aliquam.\n \rVel consequuntur temporibus et facere in. Asperiores voluptatem aliquid voluptatem qui. Omnis dolor voluptas quia quisquam vero et pariatur sed. Alias id nobis vero sed suscipit. Ut enim quo officia enim illum quis quo.",
+    createdAt: "2023-03-23T17:48:37.000Z",
+    account: "user1",
+    name: "user1",
+    avatar: "https://loremflickr.com/320/240/avatar/?random=60.90379788737232",
+    repliedCount: 1,
+    likedCount: 0,
+    isLike: false
+}
