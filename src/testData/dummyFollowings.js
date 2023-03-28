5737b08dea8de51c3afc2a3efd73ee9570f8d84e
TammyKao <a0920760272@gmail.com>
2023-03-28 14:07:29 +0800

    add dummyFollowings data

---
+++ src/testData/dummyFollowings.js
@@ -0,0 +1,99 @@
+const dummyFollowings = [
+    {
+        userId: 5,
+        name: "Dean Friesen",
+        account:"Muriel Marvin",
+        followingId: "null",
+        followingAvatar: "https://reurl.cc/XLQeQj",
+        followingName: "anonymous user",
+        followingIntro: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint ullamco est sit aliqua dolor do sit aliqua dolor do amet sint ullamco est sit aliqua dolor do  sit aliqua dolor do amet sint ullamco est sit aliqua dolor do amet sint.",
+        followingCount:0,
+        isFollowed: false
+    },
+
+    {
+        userId: 5,
+        name: "Dean Friesen",
+        account:"Muriel Marvin",
+        followingId: "null",
+        followingAvatar: "https://reurl.cc/XLQeQj",
+        followingName: "anonymous user",
+        followingIntro: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint ullamco est sit aliqua dolor do sit aliqua dolor do amet sint ullamco est sit aliqua dolor do  sit aliqua dolor do amet sint ullamco est sit aliqua dolor do amet sint.",
+        followingCount:0,
+        isFollowed: false
+    },
+
+    {
+        userId: 5,
+        name: "Dean Friesen",
+        account:"Muriel Marvin",
+        followingId: "null",
+        followingAvatar: "https://reurl.cc/XLQeQj",
+        followingName: "anonymous user",
+        followingIntro: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint ullamco est sit aliqua dolor do sit aliqua dolor do amet sint ullamco est sit aliqua dolor do  sit aliqua dolor do amet sint ullamco est sit aliqua dolor do amet sint.",
+        followingCount:0,
+        isFollowed: false
+    },
+
+    {
+        userId: 5,
+        name: "Dean Friesen",
+        account:"Muriel Marvin",
+        followingId: "null",
+        followingAvatar: "https://reurl.cc/XLQeQj",
+        followingName: "anonymous user",
+        followingIntro: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint ullamco est sit aliqua dolor do sit aliqua dolor do amet sint ullamco est sit aliqua dolor do  sit aliqua dolor do amet sint ullamco est sit aliqua dolor do amet sint.",
+        followingCount:0,
+        isFollowed: false
+    },
+
+     {
+        userId: 5,
+        name: "Dean Friesen",
+        account:"Muriel Marvin",
+        followingId: "null",
+        followingAvatar: "https://reurl.cc/XLQeQj",
+        followingName: "anonymous user",
+        followingIntro: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint ullamco est sit aliqua dolor do sit aliqua dolor do amet sint ullamco est sit aliqua dolor do  sit aliqua dolor do amet sint ullamco est sit aliqua dolor do amet sint.",
+        followingCount:0,
+        isFollowed: false
+    },
+
+    {
+        userId: 5,
+        name: "Dean Friesen",
+        account:"Muriel Marvin",
+        followingId: "null",
+        followingAvatar: "https://reurl.cc/XLQeQj",
+        followingName: "anonymous user",
+        followingIntro: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint ullamco est sit aliqua dolor do sit aliqua dolor do amet sint ullamco est sit aliqua dolor do  sit aliqua dolor do amet sint ullamco est sit aliqua dolor do amet sint.",
+        followingCount:0,
+        isFollowed: false
+    },
+
+    {
+        userId: 5,
+        name: "Dean Friesen",
+        account:"Muriel Marvin",
+        followingId: "null",
+        followingAvatar: "https://reurl.cc/XLQeQj",
+        followingName: "anonymous user",
+        followingIntro: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint ullamco est sit aliqua dolor do sit aliqua dolor do amet sint ullamco est sit aliqua dolor do  sit aliqua dolor do amet sint ullamco est sit aliqua dolor do amet sint.",
+        followingCount:0,
+        isFollowed: false
+    },
+
+    {
+        userId: 5,
+        name: "Dean Friesen",
+        account:"Muriel Marvin",
+        followingId: "null",
+        followingAvatar: "https://reurl.cc/XLQeQj",
+        followingName: "anonymous user",
+        followingIntro: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint ullamco est sit aliqua dolor do sit aliqua dolor do amet sint ullamco est sit aliqua dolor do  sit aliqua dolor do amet sint ullamco est sit aliqua dolor do amet sint.",
+        followingCount:0,
+        isFollowed: false
+    },
+]
+
+export { dummyFollowings }
