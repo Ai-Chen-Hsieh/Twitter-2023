218efed6965a685188bbb10a4b79f6ff197b2437
Ai-Chen-Hsieh <a0903109727@gmail.com>
2023-03-30 10:05:15 +0800

    修改api格式

--- src/pages/BasePage.jsx
+++ src/pages/BasePage.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react"
 import { createPortal } from "react-dom";
 import { useAuth } from "contexts/AuthContext"
 import styled from "styled-components"
-import { getPopularList } from "../api/api_popularlist";
+// import { getPopularList } from "../api/api_popularlist";
 const StyledContainer = styled.div`
   width: 100%;
   max-width: 100%;
@@ -63,27 +63,27 @@ const BasePage = ({showPopularList = true}) => {
   let navigate = useNavigate()
   
   //引入推薦者清單
-useEffect(()=>{
-  const getPopularListAsync = async () => {
-    try{
-      const response = await getPopularList();
-      //若未成功取得status不等於success，lists useState 保持為空陣列
-      if(response.status !== 'success' ){
-       return
-      } else if(response.status === 'success'){
-        setLists(()=>{
-          return[
-            ...response.data
-          ]
-        });
-      }
-    }catch(error){
-      console.error(error)
-    }
-  }
+// useEffect(()=>{
+//   const getPopularListAsync = async () => {
+//     try{
+//       const response = await getPopularList();
+//       //若未成功取得status不等於success，lists useState 保持為空陣列
+//       if(response.status === 200){
+//         setLists(()=>{
+//           return[
+//             ...response.data
+//           ]
+//         });
+//       } else {
+//         return
+//       }
+//     }catch(error){
+//       console.error(error)
+//     }
+//   }
 
-  getPopularListAsync()
-},[])
+//   getPopularListAsync()
+// },[])
 
   // 檢查是否有 token
   // 沒有 -> 登入頁
