c8b52c90b7a8a335cc2c27e5c3b2476af233d2c3
Ai-Chen-Hsieh <a0903109727@gmail.com>
2023-04-01 10:52:13 +0800

    修正editModal file

--- src/api/api_editModal.js
+++ src/api/api_editModal.js
@@ -4,9 +4,9 @@ export const editUserProfile = async (id, formData) => {
     try {
       console.log(id, formData)
         const res = await axiosInstance.put(`${baseUrl}/api/users/${id}`, formData, { 
-
-          
-          
+            headers: {
+            'Content-Type': 'multipart/form-data'
+          }
         })
         console.log(res)
         return res
